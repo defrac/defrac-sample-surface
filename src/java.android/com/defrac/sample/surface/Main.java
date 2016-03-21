6f7e2a82153128d4a3d69c407c67a260140ed59e
Joa Ebert <joa.ebert@defrac.com>
2016-03-21 14:19:21 +0100

    Update for 0.3.116

--- src/java.android/com/defrac/sample/surface/Main.java
+++ src/java.android/com/defrac/sample/surface/Main.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Build;
 import defrac.dni.Activity;
 import defrac.dni.IntentFilter;
-import defrac.dni.UsesSdk;
 import defrac.ui.Screen;
 import defrac.ui.ScreenActivity;
 
@@ -14,7 +13,6 @@ import javax.annotation.Nonnull;
  *
  */
 @Activity(label = "Surface Sample", filter = @IntentFilter(action = Intent.ACTION_MAIN, category = Intent.CATEGORY_LAUNCHER))
-@UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public final class Main extends ScreenActivity {
   @Nonnull
   @Override
